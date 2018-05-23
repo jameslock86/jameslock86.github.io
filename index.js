04d149546151c0f18db6e23fdb51f280336747ba
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2018-05-22 21:39:14 -0600

    different colors added

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 
+
 function appendIcons(){
 
   let icons = [
-    './Assets/icons/react.png',
-    './Assets/icons/reactNative.png',
-    './Assets/icons/jQuery.gif',
-    './Assets/icons/htmlFive.png',
-    './Assets/icons/CSSThree.png',
-    './Assets/icons/aws.png',
-    './Assets/icons/bootstrap.png',
-    './Assets/icons/materialize.png',
-    './Assets/icons/node.png',
-    './Assets/icons/mongodb.png',
-    './Assets/icons/node.png',
-    './Assets/icons/heroku.png',
-    './Assets/icons/knex.png',
-    './Assets/icons/postman.png',
-    './Assets/icons/wordpress.png',
-    './Assets/icons/axios.png',
-    './Assets/icons/api.png'
+    ['purple', './Assets/icons/react.png'],
+    ['blue', './Assets/icons/reactNative.png'],
+    ['pink', './Assets/icons/jQuery.gif'],
+    ['green', './Assets/icons/htmlFive.png'],
+    ['pink', './Assets/icons/CSSThree.png'],
+    ['green', './Assets/icons/aws.png'],
+    ['blue', './Assets/icons/bootstrap.png'],
+    ['pink', './Assets/icons/materialize.png'],
+    ['pink', './Assets/icons/node.png'],
+    ['pink', './Assets/icons/mongodb.png'],
+    ['purple', './Assets/icons/node.png'],
+    ['blue', './Assets/icons/heroku.png'],
+    ['pink', './Assets/icons/knex.png'],
+    ['green', './Assets/icons/postman.png'],
+    ['pink', './Assets/icons/wordpress.png'],
+    ['blue', './Assets/icons/axios.png'],
+    ['purple', './Assets/icons/api.png']
   ]
 
   icons.forEach((icon) => {
-    $('#icons').append(`
-      <div class="iconWrapper col-md-2">
-          <img class="icon" src="${icon}">
+      $('#icons').append(`
+      <div class="iconWrapper col-md-2 ${icon[0]}">
+          <img class="icon" src="${icon[1]}">
       </div>
     `)
   })
